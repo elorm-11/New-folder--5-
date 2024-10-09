3982c9e4a3de220c1e996bcfe3c363caaab4bb96
elorm-11 <elormkovey@gmail.com>
2024-10-09 20:12:27 +0000

    1commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# checking for alphabets
+string = 'Ayush'
+print(string.isalpha())
+
+string = 'Ayush0212'
+print(string.isalpha())
+
+# checking if space is an alphabet
+string = 'Ayush Saxena'
+print( string.isalpha())
